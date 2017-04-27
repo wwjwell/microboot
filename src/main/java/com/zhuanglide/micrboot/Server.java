1a8911ac664473dc9d3ee113d59397f36304049a
wwj <wangweijie@weidian.com>
2017-04-27 17:34:30 +0800

    修改配置

--- src/main/java/com/zhuanglide/micrboot/Server.java
+++ src/main/java/com/zhuanglide/micrboot/Server.java
@@ -24,6 +24,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 
 import java.nio.charset.Charset;
+import java.util.concurrent.Executor;
 
 /**
  * http server
@@ -32,34 +33,31 @@ import java.nio.charset.Charset;
 public class Server implements ApplicationContextAware,InitializingBean {
     private Logger logger = LoggerFactory.getLogger(Server.class);
     private String charset = "UTF-8"; //默认编码
-    private boolean useEpoll = false;
+    private boolean useEpoll = true;
     private int port;
-    //系统处理线程数，默认为当前CPU的核心数的2倍
-    private int threadNum = Runtime.getRuntime().availableProcessors() * 2;
-    private int bossThreads = threadNum ; //netty boss 线程
-    private int workThreads =  2 * bossThreads; //netty work线程
+
+    private static final int DEFAULT_EVENT_LOOP_THREADS;
+    static {
+        DEFAULT_EVENT_LOOP_THREADS = Math.max(1, Runtime.getRuntime().availableProcessors() * 2);
+    }
+    private int threadNum;
     private int maxLength = 65536; //http报文最大长度
     private boolean useChunked = false;
+    private int bossThreadNum;
+    private int workerThreadNum;
+    private Executor executor;
     private ApplicationContext context;
     private HttpSimpleChannelHandle httpSimpleChannelHandle;
     private EventLoopGroup bossGroup;
     private EventLoopGroup workerGroup;
-
+    private Class<? extends ServerChannel> socketChannelClass;
     /**
      * Http服务启动
      * 系统异步线程方式启动起来
      */
     public void start(){
-        Class<? extends ServerChannel> socketChannelClass;
-        if(epollAvailable()) {
-            bossGroup = new EpollEventLoopGroup(bossThreads);
-            workerGroup = new EpollEventLoopGroup(workThreads);
-            socketChannelClass = EpollServerSocketChannel.class;
-        }else{
-            bossGroup = new NioEventLoopGroup(bossThreads);
-            workerGroup = new NioEventLoopGroup(workThreads);
-            socketChannelClass = NioServerSocketChannel.class;
-        }
+        intEventGroupAndChannel();
+
         try {
             ServerBootstrap b = new ServerBootstrap();
             b.group(bossGroup, workerGroup).channel(socketChannelClass)
@@ -79,7 +77,7 @@ public class Server implements ApplicationContextAware,InitializingBean {
             logger.info("server start at port {}",port);
             ChannelFuture f = b.bind(port).sync();
             f.channel().closeFuture().sync();
-        } catch (InterruptedException e) {
+        } catch (Exception e) {
             logger.error("", e);
         } finally {
             shutdown();
@@ -91,14 +89,63 @@ public class Server implements ApplicationContextAware,InitializingBean {
         start();
     }
 
+
+    /**
+     * 优雅的关闭
+     */
     public void shutdown(){
-        if (null != bossGroup) {
-            bossGroup.shutdownGracefully();
-        }
-        if (null != workerGroup) {
-            workerGroup.shutdownGracefully();
+        try {
+            if (null != bossGroup) {
+                bossGroup.shutdownGracefully();
+            }
+            if (null != workerGroup) {
+                workerGroup.shutdownGracefully();
+            }
+        } catch (Exception e) {
+            logger.error("", e);
         }
     }
+
+
+    /**
+     * 初始化
+     * @param context
+     */
+    protected void initStrategies(ApplicationContext context) {
+        try {
+            httpSimpleChannelHandle = context.getBean(HttpSimpleChannelHandle.class);
+        } catch (NoSuchBeanDefinitionException e) {
+            httpSimpleChannelHandle = context.getAutowireCapableBeanFactory().createBean(HttpSimpleChannelHandle.class);
+            httpSimpleChannelHandle.setCharset(Charset.forName(charset));
+        }
+    }
+
+    /**
+     * 初始化eventGroup 初始化channel
+     */
+    private void intEventGroupAndChannel(){
+        if(epollAvailable()) {
+            if(null == getExecutor()) {
+                bossGroup = new EpollEventLoopGroup(getBossThreadNum());
+                workerGroup = new EpollEventLoopGroup(getWorkerThreadNum());
+            }else{
+                bossGroup = new EpollEventLoopGroup(getBossThreadNum(), getExecutor());
+                workerGroup = new EpollEventLoopGroup(getWorkerThreadNum(), getExecutor());
+            }
+            socketChannelClass = EpollServerSocketChannel.class;
+        }else{
+            if(null == getExecutor()) {
+                bossGroup = new NioEventLoopGroup(getBossThreadNum());
+                workerGroup = new NioEventLoopGroup(getWorkerThreadNum());
+            }else{
+                bossGroup = new NioEventLoopGroup(getBossThreadNum(), getExecutor());
+                workerGroup = new NioEventLoopGroup(getWorkerThreadNum(), getExecutor());
+            }
+            socketChannelClass = NioServerSocketChannel.class;
+        }
+    }
+
+
 
     public String getCharset() {
         return charset;
@@ -128,29 +175,17 @@ public class Server implements ApplicationContextAware,InitializingBean {
         this.threadNum = threadNum;
     }
 
-    public void setBossThreads(int bossThreads) {
-        this.bossThreads = bossThreads;
-    }
-
-    public void setWorkThreads(int workThreads) {
-        this.workThreads = workThreads;
-    }
-
     public void setUseEpoll(boolean useEpoll) {
         this.useEpoll = useEpoll;
     }
 
     public int getThreadNum() {
+        if (threadNum < 1) {
+            threadNum = DEFAULT_EVENT_LOOP_THREADS;
+        }
         return threadNum;
     }
 
-    public int getBossThreads() {
-        return bossThreads;
-    }
-
-    public int getWorkThreads() {
-        return workThreads;
-    }
 
     public boolean isUseChunked() {
         return useChunked;
@@ -163,18 +198,37 @@ public class Server implements ApplicationContextAware,InitializingBean {
     private boolean epollAvailable(){
         return useEpoll && Epoll.isAvailable();
     }
-    /**
-     * 初始化
-     * @param context
-     */
-    protected void initStrategies(ApplicationContext context) {
-        try {
-            httpSimpleChannelHandle = context.getBean(HttpSimpleChannelHandle.class);
-        } catch (NoSuchBeanDefinitionException e) {
-            httpSimpleChannelHandle = context.getAutowireCapableBeanFactory().createBean(HttpSimpleChannelHandle.class);
-            httpSimpleChannelHandle.setCharset(Charset.forName(charset));
-        }
+
+    public Executor getExecutor() {
+        return executor;
     }
+
+    public void setExecutor(Executor executor) {
+        this.executor = executor;
+    }
+
+    public int getBossThreadNum() {
+        if (bossThreadNum < 1) {
+            bossThreadNum = getThreadNum();
+        }
+        return bossThreadNum;
+    }
+
+    public void setBossThreadNum(int bossThreadNum) {
+        this.bossThreadNum = bossThreadNum;
+    }
+
+    public int getWorkerThreadNum() {
+        if (workerThreadNum < 1) {
+            workerThreadNum = getBossThreadNum() * 2;
+        }
+        return workerThreadNum;
+    }
+
+    public void setWorkerThreadNum(int workerThreadNum) {
+        this.workerThreadNum = workerThreadNum;
+    }
+
 
     @Override
     public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
